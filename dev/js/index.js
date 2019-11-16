5bf22f079c578349d69d4139c970a62bfbf27285
hadashi <hadashi.heitai@gmail.com>
2019-11-16 11:13:54 +0100

    Code formatting

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,8 +1,8 @@
 import Person from "./Person.js";
+import Item from "./Item.js";
 import { elementMatches } from "./utils.js";
 import { elementClosest } from "./utils.js";
-import Item from "./Item.js";
- 
+
 var user = new Person;
 user.money = 1000;
 
@@ -18,118 +18,118 @@ const itemTypes = {
 
 Object.freeze(itemTypes);
 
-
-$(document).ready(function(){ 
+$(document).ready(function () {
   updateFunds();
-
 });
 
-function updateFunds(){
+function updateFunds() {
   $("#user-money").html(user.money);
   $("#vendor-money").html(vendor.money);
 }
-  
+
 $('.user-inventory').on('click', function (e) {
   if (elementMatches(e.target, '#use-item')) {
-    removeItem(e,userInventory);
+    removeItem(e, userInventory);
   }
 });
 
 function removeItem(e, inventory) {
   var elem = elementClosest(e.target, '.item');
-  inventory.hide(elem, {onFinish: function (items) {
-    var item = items[0];
-    inventory.remove(item, {removeElements: true});
-  }});
+  inventory.hide(elem, {
+    onFinish: function (items) {
+      var item = items[0];
+      inventory.remove(item, { removeElements: true });
+    }
+  });
 }
- 
+
 var vendorInventory = new Muuri('.vendor-inventory', {
-    items: generateItems(56),
-    dragEnabled: true,
-    dragStartPredicate: function (item, event) {
-        //dragable only if price is not bigger than funds
-        var element = item._element;
-        var price = $(element).data("price");
-        if (price > user.money) {
-            return false;
-        }
-        return Muuri.ItemDrag.defaultStartPredicate(item, event);
-    },
-    dragContainer: document.body,
-    dragSort: function () {
-      return [vendorInventory, userInventory]
-    },
-    dragPlaceholder: {
-        enabled: true,
-        duration: 400,
-        createElement: function (item) {
-          return item.getElement().cloneNode(true);
-        }
-      },
-      dragReleaseDuration: 400,
-      dragReleseEasing: 'ease'
-  })
-  .on('beforeSend', function (data) {
-    var element = data.item._element;
+  items: generateItems(56),
+  dragEnabled: true,
+  dragStartPredicate: function (item, event) {
+    //dragable only if price is not bigger than funds
+    var element = item._element;
     var price = $(element).data("price");
-    if(price < user.money){
-        user.money = user.money - price;
-        vendor.money = +vendor.money + +price;
-        updateFunds();
+    if (price > user.money) {
+      return false;
     }
-  });
-  
-  var userInventory = new Muuri('.user-inventory', {
-    items: generateItems(2),
-    dragEnabled: true,
-    dragStartPredicate: function (item, event) {
-        //dragable only if price is not bigger than funds
-        var element = item._element;
-        var price = $(element).data("price");
-        if (price > vendor.money) {
-            return false;
-        }
-        return Muuri.ItemDrag.defaultStartPredicate(item, event);
-    },
-    dragContainer: document.body,
-    dragSort: function () {
-      return [vendorInventory, userInventory]
-    },
-    dragPlaceholder: {
-        enabled: true,
-        duration: 400,
-        createElement: function (item) {
-          return item.getElement().cloneNode(true);
-        }
-      },
-      dragReleaseDuration: 400,
-      dragReleseEasing: 'ease'
-  })
+    return Muuri.ItemDrag.defaultStartPredicate(item, event);
+  },
+  dragContainer: document.body,
+  dragSort: function () {
+    return [vendorInventory, userInventory]
+  },
+  dragPlaceholder: {
+    enabled: true,
+    duration: 400,
+    createElement: function (item) {
+      return item.getElement().cloneNode(true);
+    }
+  },
+  dragReleaseDuration: 400,
+  dragReleseEasing: 'ease'
+})
   .on('beforeSend', function (data) {
     var element = data.item._element;
     var price = $(element).data("price");
-    if(price < vendor.money){
-        vendor.money = vendor.money - price;
-        user.money = +user.money + +price;
-        updateFunds();
+    if (price < user.money) {
+      user.money = user.money - price;
+      vendor.money = +vendor.money + +price;
+      updateFunds();
     }
   });
 
-  
+var userInventory = new Muuri('.user-inventory', {
+  items: generateItems(2),
+  dragEnabled: true,
+  dragStartPredicate: function (item, event) {
+    //dragable only if price is not bigger than funds
+    var element = item._element;
+    var price = $(element).data("price");
+    if (price > vendor.money) {
+      return false;
+    }
+    return Muuri.ItemDrag.defaultStartPredicate(item, event);
+  },
+  dragContainer: document.body,
+  dragSort: function () {
+    return [vendorInventory, userInventory]
+  },
+  dragPlaceholder: {
+    enabled: true,
+    duration: 400,
+    createElement: function (item) {
+      return item.getElement().cloneNode(true);
+    }
+  },
+  dragReleaseDuration: 400,
+  dragReleseEasing: 'ease'
+})
+  .on('beforeSend', function (data) {
+    var element = data.item._element;
+    var price = $(element).data("price");
+    if (price < vendor.money) {
+      vendor.money = vendor.money - price;
+      user.money = +user.money + +price;
+      updateFunds();
+    }
+  });
+
+
 //init
-  function generateItems(amount) {
-    let ret = []; 
-    let keys = Object.keys(itemTypes);
+function generateItems(amount) {
+  let ret = [];
+  let keys = Object.keys(itemTypes);
 
-    for (var i = 0; i < amount; i++) {
-      let randomType = itemTypes[keys[ keys.length * Math.random() << 0]];
-      let randomQuantity = 1 + Math.floor(Math.random() * 100);
-      let randomPrice = 100 + Math.floor(Math.random() * 2000);
+  for (var i = 0; i < amount; i++) {
+    let randomType = itemTypes[keys[keys.length * Math.random() << 0]];
+    let randomQuantity = 1 + Math.floor(Math.random() * 100);
+    let randomPrice = 100 + Math.floor(Math.random() * 2000);
 
-      let item = new Item(i, randomType, randomType, randomPrice, 'desc');
-      let itemTemplate = item.getItemTemplate();
+    let item = new Item(i, randomType, randomType, randomPrice, 'desc');
+    let itemTemplate = item.getItemTemplate();
 
-      ret.push(itemTemplate);
-      }
-     return ret;
+    ret.push(itemTemplate);
   }
+  return ret;
+}
